db22a77279b899e0b2d0e071e3339ac2ab469eed
Emre <emrekurtt1655@gmail.com>
2022-03-29 23:12:46 +0200

    add: topic sort functions added

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -6,16 +6,29 @@ import Modal from "../modal/modal";
 import "./sidebar.css";
 
 export default function Sidebar() {
+  let sortedCountTopics;
+  let sortedDateTopics;
   const { authReducer, topicReducer } = useSelector((state) => state);
   const [open, setOpen] = useState(false);
+  const [lastAdded, setLastAdded] = useState(false);
+  const handleLastAdded = () => setLastAdded(!lastAdded);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  const user = authReducer?.user
+  const user = authReducer?.user;
   const topics = topicReducer?.data;
-  const sortedTopics = topics?.sort(function (topic1, topic2) {
-    return topic2._count.posts - topic1._count.posts;
+
+  const sorted2count = topics?.sort(function (topic1, topic2) {
+    return topic2?._count.posts - topic1?._count.posts;
   });
+
+  sortedCountTopics = sorted2count?.map((t) => t);
+  const sorted2date = topics?.sort(function (topic1, topic2) {
+    let dateTopic1 = new Date(topic1.createdAt);
+    let dateTopic2 = new Date(topic2.createdAt);
+    return dateTopic2 - dateTopic1;
+  });
+  sortedDateTopics = sorted2date?.map((t) => t);
 
   return (
     <div>
@@ -23,25 +36,33 @@ export default function Sidebar() {
       <div className="sidebar">
         <div className="sidebarItem">
           <span className="sidebarTitle">
-            Topic <i className="fas fa-angle-double-right"></i>
+            Topic{" "}
+            <i
+              className="fas fa-angle-double-right"
+              onClick={handleLastAdded}
+            ></i>
           </span>
-          {sortedTopics &&
-            sortedTopics?.slice(0,9)?.map((t) => (
-              <div className="titleinfoSide" key= {t?.topicId}>
-                <i className="fab fa-buffer"></i>
-                <p className="titleInfo"> {t?.text} </p>
-              </div>
-            ))}
+          {topics && lastAdded
+            ? sortedDateTopics?.slice(0, 9)?.map((t) => (
+                <div className="titleinfoSide" key={t?.topicId}>
+                  <i className="fab fa-buffer"></i>
+                  <p className="titleInfo"> {t?.text} </p>
+                </div>
+              ))
+            : sortedCountTopics?.slice(0, 9)?.map((t) => (
+                <div className="titleinfoSide" key={t?.topicId}>
+                  <i className="fab fa-buffer"></i>
+                  <p className="titleInfo"> {t?.text} </p>
+                </div>
+              ))}
 
           <span className="">
-            { user?.isTopicCreator === 'true'  && <div onClick={handleOpen} className="buttonAddTopic">
-              <i className="fab fa-buffer"></i>
-              <span 
-              style={{ margin: "5px" }}
-              >
-                Add Topic
-              </span>
-            </div>}
+            {user?.isTopicCreator === "true" && (
+              <div onClick={handleOpen} className="buttonAddTopic">
+                <i className="fab fa-buffer"></i>
+                <span style={{ margin: "5px" }}>Add Topic</span>
+              </div>
+            )}
           </span>
         </div>
         <div className="sidebarItem">
@@ -91,20 +112,20 @@ export default function Sidebar() {
             <div>
               <ul>
                 <li>
-                  <a >
+                  <a>
                     <span></span>
                     <span className="fa fa-facebook"></span>
                   </a>
                 </li>
                 <li>
-                  <a >
+                  <a>
                     <span></span>
                     <span></span>
                     <span className="fa fa-twitter"></span>
                   </a>
                 </li>
                 <li>
-                  <a >
+                  <a>
                     <span></span>
                     <span></span>
                     <span></span>
@@ -112,7 +133,7 @@ export default function Sidebar() {
                   </a>
                 </li>
                 <li>
-                  <a >
+                  <a>
                     <span></span>
                     <span></span>
                     <span></span>
