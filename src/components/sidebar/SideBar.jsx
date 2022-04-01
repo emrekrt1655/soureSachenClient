99c2a416d02f8f10dcdb600ac357e22c4c537017
faris <faris38kanbur@gmail.com>
2022-04-01 16:06:59 +0200

    add mostliked func

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -88,6 +88,7 @@ export default function Sidebar() {
             )}
           </span>
         </div>
+
         <div className="sidebarItem">
           <div className="sidebarItem">
             <span className="sidebarTitle">THE MOST LIKED</span>
--- src/components/sidebar/likedposts/LikedPosts.jsx
+++ src/components/sidebar/likedposts/LikedPosts.jsx
@@ -1,30 +1,51 @@
 import { Link } from "react-router-dom";
 import "./likedposts.css";
+import { useSelector } from "react-redux";
 
-export default function Post({img}) {
+export default function Post({ img }) {
+  const { postReducer, topicReducer } = useSelector((state) => state);
+  const posts = postReducer?.data;
+  const topics = topicReducer?.data;
+
+  console.log("postReducer :>> ", posts);
+  let newList = [];
+  // console.log(posts);
+
+  posts?.map((post) => {
+    return newList.push({
+      postId: post.postId,
+      count: post._count.comments + post._count.likes,
+    });
+  });
+
+  newList?.sort(function (a, b) {
+    return b.count - a.count;
+  });
+  console.log("newList :>> ", newList);
+
+  const post = posts?.find((post) => post.postId === newList[0].postId);
+  console.log(post);
+
+  const topic = topics?.find((t) => {
+    return t.topicId === post?.postTopicId;
+  });
+  console.log("topic :>> ", topic);
+
   return (
     <div className="postlked">
-      <img
-        className="postImgliked"
-        src="https://images.pexels.com/photos/4916559/pexels-photo-4916559.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-        alt=""
-      />
-    <div className="posttextinfo">
+      {post?.image && (
+        <img className="postImgliked" src={post?.image} alt="mostLikedpost" />
+      )}
+      <div className="posttextinfo">
         <div className="postInfo">
           <span className="postTitle">
             <Link to="/post/abc" className="link">
-              Lorem ipsum dolor sit amet
+              {topic?.text}
             </Link>
           </span>
           <hr />
-          
         </div>
-        <p className="postDesc">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
-          officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
-          fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
-          atque, exercitationem quibusdam, reiciendis odio laboriosam?
-        </p>
+        <p className="postDesc">{post?.text}</p>
 
         <div className="extrainfo">
           <div className="postCats">
@@ -40,15 +61,17 @@ export default function Post({img}) {
             </span>
           </div>
           <div>
-           <span className="postDate">1 hour ago</span>
+            <span className="postDate">
+              {new Date(post?.createdAt).toDateString()}
+            </span>
           </div>
         </div>
         <div>
           <i className="far fa-hand-spock"></i>
-          <i className="far fa-comment"  style={{margin:"2%"}}></i>
-          <i className="fab fa-slideshare"  ></i>
+          <i className="far fa-comment" style={{ margin: "2%" }}></i>
+          <i className="fab fa-slideshare"></i>
         </div>
-      </div>  
+      </div>
     </div>
   );
 }
