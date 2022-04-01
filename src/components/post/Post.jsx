74e7f6acf9a49ac4104ddc201b3e94bbf13452c7
faris <faris38kanbur@gmail.com>
2022-04-01 19:07:52 +0200

    add post liked and comment number

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,5 +1,9 @@
 import "./post.css";
 import { Link } from "react-router-dom";
+import ShareRoundedIcon from "@mui/icons-material/ShareRounded";
+import RecommendRoundedIcon from "@mui/icons-material/RecommendRounded";
+import MarkChatUnreadIcon from "@mui/icons-material/MarkChatUnread";
+import Tooltip from "@mui/material/Tooltip";
 
 export default function Post({ post, topicData }) {
   const topic = topicData?.filter((topic) => {
@@ -10,17 +14,48 @@ export default function Post({ post, topicData }) {
   const topicId = topic?.map(({ topicId }) => topicId);
 
   return (
-    <div className="post">
-      {post.image && <img className="postImg" src={post.image} alt="post" />}
-      <div className="postInfo">
-        <Link to={`topic/${topicId}`}>
-        
-        <p className="postTitle">{topicText}</p>
-        </Link>
-      <p className="postDesc">{post?.text}</p>
-        <span className="postDate">
-          {new Date(post.createdAt).toDateString()}
-        </span>
+    <div className="postContent">
+      <div className="post">
+        {post.image && <img className="postImg" src={post.image} alt="post" />}
+        <div className="postInfoHomePage">
+          <Link to={`topic/${topicId}`}>
+            <p className="postTitle">{topicText}</p>
+          </Link>
+          <p className="postDesc">{post?.text}</p>
+          <span className="postDate">
+            {new Date(post.createdAt).toDateString()}
+          </span>
+        </div>
+      </div>
+      {/* added post like and comment number acording to count */}
+      <div className="postIcons">
+        <div>
+          <Tooltip
+            title={
+              `${post?._count?.likes}` <= 1
+                ? `${post?._count?.likes} Like`
+                : `${post?._count?.likes} Likes`
+            }
+          >
+            <RecommendRoundedIcon />
+          </Tooltip>
+        </div>
+        <div>
+          <Tooltip
+            title={
+              `${post?._count?.comments}` <= 1
+                ? `${post?._count?.likes} Comment`
+                : `${post?._count?.likes} Comments`
+            }
+          >
+            <MarkChatUnreadIcon style={{ margin: "0 2% " }} />
+          </Tooltip>
+        </div>
+        <div>
+          <Tooltip title="Share">
+            <ShareRoundedIcon style={{ margin: "0 2% " }} />
+          </Tooltip>
+        </div>
       </div>
     </div>
   );
